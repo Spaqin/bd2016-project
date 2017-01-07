389d978a484e41e7449e013283ae11f56cbc18d6
Tomasz Jakubczyk <sigrond93@gmail.com>
2017-01-07 14:18:44 +0100

    created project

---
+++ src/adminGUI/AdminGUI.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package adminGUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+/**
+ * @author x
+ *
+ */
+public class AdminGUI
+{
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					AdminGUI window = new AdminGUI();
+					window.frame.setVisible(true);
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AdminGUI()
+	{
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize()
+	{
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
